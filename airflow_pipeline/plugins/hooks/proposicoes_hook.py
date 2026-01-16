c2e1ebfed56c634bfcceb87ec2cf811b14679c0c
Abner Freitas <defreitasabner@gmail.com>
2026-01-16 18:52:22 -0300

    Implementadas DAGs para processo de Extract e Load de proposições.

--- airflow_pipeline/plugins/hooks/proposicoes_hook.py
+++ airflow_pipeline/plugins/hooks/proposicoes_hook.py
@@ -3,26 +3,38 @@ from airflow.providers.http.hooks.http import HttpHook
 
 
 class ProposicoesHook(HttpHook):
-    def __init__(self, end_time, start_time):
-        self.end_time = end_time
+    def __init__(self, start_time, end_time):
         self.start_time = start_time
+        self.end_time = end_time
         self.DATE_FORMAT = '%d/%m/%Y'
         
         self.conn_id = 'camara_api'
         super().__init__(http_conn_id = self.conn_id)
 
     def create_url(self):
-        return f'{self.base_url}/Proposicoes.asmx/ListarProposicoes'
+        return f'{self.base_url}/SitCamaraWS/Proposicoes.asmx/ListarProposicoes'
 
     def connect(self, url, session):
         params = {
             'sigla': 'PL',
-            'ano': self.start_time.date().year,
+            'numero': '',
+            'ano': self.start_time.year,
             'datApresentacaoIni': self.start_time.strftime(self.DATE_FORMAT),
             'datApresentacaoFim': self.end_time.strftime(self.DATE_FORMAT),
+            'IdTipoAutor': '',
+            'ParteNomeAutor': '',
+            'SiglaPartidoAutor': '',
+            'SiglaUfAutor': '',
+            'GeneroAutor': '',
+            'IdSituacaoProposicao': '',
+            'IdOrgaoSituacaoProposicao': '',
+            'EmTramitacao': '',
+            'codEstado': '',
+            'codOrgaoEstado': ''
         }
         request = requests.Request('GET', url, params = params)
         prep = session.prepare_request(request)
+        self.log.info(f'Connecting to: {prep.url}')
         return self.run_and_check(session, prep, {})
     
     def get_response(self, url, session):
--- airflow_pipeline/plugins/operators/extract_proposicoes_operator.py
+++ airflow_pipeline/plugins/operators/extract_proposicoes_operator.py
@@ -1,16 +1,25 @@
+from datetime import timedelta
+
 from airflow.models import BaseOperator
 
 from hooks.proposicoes_hook import ProposicoesHook
 from transformers.proposicoes_transformer import ProposicoesTransformer
+from hooks.mongo_hook import MongoHook
 
 
 class ExtractProposicoesOperator(BaseOperator):
-    def __init__(self, end_time, start_time, **kwargs):
-        self.end_time = end_time
-        self.start_time = start_time
+
+    def __init__(self, database, collection, **kwargs):
+        self.database = database
+        self.collection = collection
         super().__init__(**kwargs)
 
     def execute(self, context):
-        hook = ProposicoesHook(self.end_time, self.start_time)
-        response = hook.run()
-        return ProposicoesTransformer().xml_to_json(response)
+        start_time = context['data_interval_start']
+        end_time = context['data_interval_end'] - timedelta(days = 1)
+        http_hook = ProposicoesHook(start_time, end_time)
+        response = http_hook.run()
+        proposicoes = ProposicoesTransformer().xml_to_json(response)
+        mongo_hook = MongoHook()
+        inserted_ids = mongo_hook.insert_many(self.database, self.collection, proposicoes)
+        self.log.info(f"Loaded {len(inserted_ids)} Proposições to MongoDB")
